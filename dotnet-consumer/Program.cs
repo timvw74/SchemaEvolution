f489a77b4a9e1e0caa652846950ad8fab1ea997e
Tim van Wijk <tim.vanwijk@just-eat.com>
2021-02-26 15:54:47 +0000

    String

---
+++ dotnet-consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace dotnet_consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig()
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "dotnet-users-string",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build()) {
+
+                consumer.Subscribe("users-string");
+
+                while (true)
+                {
+                    var consumeResult = consumer.Consume();
+                    Console.WriteLine($"Received User: {consumeResult.Message.Value}");
+                }            
+            }
+        }
+    }
+}
---
+++ dotnet-producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace dotnet_producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+
+            var producer = new ProducerBuilder<Null, string>(config).Build();
+
+            while (true)
+            {
+                var username = Faker.Name.First(); ;
+
+                Console.WriteLine($"Creating user {username}");
+
+                producer.Produce("users-string", new Message<Null, string>()
+                {
+                    Value = username
+                });
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
